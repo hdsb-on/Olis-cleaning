7e71d4f6cd8ef624a28cad0c2bb0ec5daaa08dfa
Mohammin <inasriyaz.mohammed@ontario.ca>
2024-07-05 10:16:16 -0400

    Inas-Glucose v2

--- cleaning_glucose.py
+++ cleaning_glucose.py
@@ -3,58 +3,6 @@ import pandas as pd
 import numpy as np
 import re
 
-#%%
-extracode = '4269-7'
-df = pd.read_sas("S:\\HDSB\\Projects\\Olis_cleaning\\indata\\olis_glucose_top5000.sas7bdat", encoding="latin1")
-
-#%%
-data=df
-
-#%%
-# Filter out non-numeric values in 'value' column
-df['upvalue'] = df['VALUE'].str.upper()
-df['value_encoded'] = df['VALUE']
-
-# Creating obs_sup_0 equivalent
-obs_sup_0 = df[df['VALUE'].str.contains(r'[^\d\s.]')]
-
-# Creating obs_sup equivalent
-excluded_strings = [
-    'TEST NOT PERFORMED', 'DISREGARD', 'CORRECTED ON', 'NOT VALID', 
-    'ORIGINAL GLUCOSE', 'FLUID', 'DIABETES', 'NO SPECIMEN RECEIVED',
-    'NO SAMPLE RECEIVED', 'NOT RECEIVED', 'UNABLE TO COLLECT',
-    'NOT SUFFICIENT QUANTITY', 'INSUFFICIENT SAMPLE', 
-    'INSUFFICIENT QUANTITY', 'SPECIMEN UNSATISFACTORY', 'MISLABELLED',
-    'UNLABELLED SAMPLE', 'HAEMOLYZED', 'HEMOLYZED', 'HEMOLYSED',
-    'QUERY SPECIMEN INTEGRITY', 'CONTAMINATION', 'CONTAMINATED',
-    'STABILITY LIMIT EXCEEDED', 'AIR BUBBLES IN SAMPLE', 'WRONG TUBE',
-    'FAILED TO MEET THE REQUIREMENTS', 'DISCARDED', 'NOT PROCESSED',
-    'UNABLE TO PROCESS', 'REJECTION', 'SPECIMEN REJECTED', 
-    'HAS BEEN REJECTED', 'REJECTED SPECIMEN', 'UNSUITABLE', 'CANCELLED',
-    'CANCEL', 'UNABLE TO PERFORM', 'CANNOT BE PERFORMED', 'NOT PERFORMED',
-    'HAS NOT BEEN PERFORMED', 'NOT BEEN SIGNED', 'ERROR', 'NOT REPORTED',
-    'WRONG TEST CODE', 'RESULT UNAVAILABLE', 'NOT AVAILABLE', 
-    'NOT BE AVAILABLE', 'DELETE', 'INVALID', 'QUESTIONABLE RESULTS', 
-    'BELOW THE MINIMUM ACCEPTABLE LEVEL', 'DUPLICATE REQUEST', 
-    'DUPLICATE ORDER', 'REQUEST A REDRAW'
-]
-
-obs_sup = obs_sup_0[
-    (obs_sup_0['OBSERVATIONCODE'] != extracode) & 
-    obs_sup_0['upvalue'].apply(lambda x: any(digit.isdigit() for digit in x)) & 
-    ~obs_sup_0['upvalue'].apply(lambda x: any(substring in x for substring in excluded_strings))
-]
-obs_sup=obs_sup.drop(columns=['VALUE'])
-
-
-# %%
-obs_sup2 = df[
-    (df['OBSERVATIONCODE'] == extracode) &  # Filtering by ObservationCode
-    (df['upvalue'].str.isdigit()) &  # Checking if 'upvalue' contains any digit
-    ~df['upvalue'].str.contains(r'TEST NOT PERFORMED|DISREGARD|CORRECTED ON|NOT VALID|ORIGINAL GLUCOSE|FLUID|DIABETES')  # Excluding specific keywords
-]
-obs_sup2 = obs_sup2.reset_index(drop=True)
-
 def check_and_call_missing(row):
     value = row['value_encoded']
     upvalue = row['upvalue']
@@ -79,8 +27,6 @@ def check_and_call_missing(row):
         row['upvalue'] = None
     
     return row
-
-obs_sup_ = obs_sup.apply(check_and_call_missing, axis=1)
 
 
 # %%
@@ -137,12 +83,6 @@ def process_row(row):
                 Value_derived = None
         else:
             Value_derived = None
-
-
-
-
-
-
 
     elif re.match(r'^\d{1,3}\.\d *(R|\(|MMOL)', upvalue):
         re_pattern = re.compile(r'^(\d{1,3}\.\d) *(R|\(|MMOL)')
@@ -234,39 +174,11 @@ def process_row(row):
         Value_derived = None
     elif "2 SAMPLES WERE RECEIVED WITH THE REQUISITION FOR\\.BR\\2 HOUR 75 GM OGTT\\.BR\\TUBES WERE LABELLED: PANZO ELLIDUS, AXVIERA\\.BR\\1979/03/24\\.BR\\MARCH 6, 2014\\.BR\\THERE WAS NO INDICATION ON THE TUBES WHICH TUBE\\.BR\\WAS FASTING OR 2 HOUR AND NO TIMES ON THE TUBES\\.BR\\IN ORDER TO IDENTIFY WE LABELLED THE TUBES A AND B\\.BR\\TUBE A PLASMA GLUCOSE RESULT WAS 5.4 MMOL/L\\.BR\\TUBE B PLASMA GLUCOSE RESULT WAS 4.7 MMOL/L\\.BR\\WE ARE UNABLE TO IDENTIFY WHICH TUBE RECEIVED WAS\\.BR\\THE FASTING OR PRE-DOSE SAMPLE AND WHICH TUBE\\.BR\\WAS THE 2 HOUR OR POST-DOSE SAMPLE\\.BR\\MR  2014/MAR/07" in upvalue:
         Value_derived = None
-
-
     return pd.Series({'Value_derived': Value_derived, 'SubValue1': SubValue1})
 
-clean = obs_sup_.apply(process_row, axis=1)
-clean = pd.concat([obs_sup_, clean], axis=1)
 
 # %%
-clean
-
-# %%
-
-df = pd.read_sas("S:\\HDSB\\Projects\\Olis_cleaning\\indata\\olis_glucose_top5000.sas7bdat", encoding="latin1")
-df=df.rename(columns={'HCN_ENCRYPTED':'HCN_ENCRYPTED',
-                      'ORDERSID':'ORDERSID',
-                      'OBSERVATIONCODE':'OBSERVATIONCODE',
-                      'OBSERVATIONDATETIME':'OBSERVATIONDATETIME',
-                      'OBSERVATIONRELEASETS':'OBSERVATIONRELEASETS',
-                      'VALUETYPE':'VALUETYPE',
-                      'UNITS':'UNITS',
-                      'REFRANGE':'REFRANGE',
-                      'OBSERVATIONPOSINTESTREQUEST':'OBSERVATIONPOSINTESTREQUEST',
-                      'TESTREQUESTPOSITIONINORDER':'TESTREQUESTPOSITIONINORDER'})
-
-clean_date = pd.merge(clean, df[['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST','OBSERVATIONDATETIME']], 
-                      how='left', 
-                      on=['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST'])
-
-
-# %%
-clean1 = clean.copy()
-
-def process_row(row):
+def process_row2(row):
     if pd.isnull(row['Value_derived']) and 'ORIGINAL' in row['upvalue']:
         re_pattern = r"(\d{2})-(\w{3})-(\d{2})"
         match = re.search(re_pattern, row['upvalue'])
@@ -285,87 +197,141 @@ def process_row(row):
                     row['Value_derived'] = value
     return row
 
-# Apply the function row-wise
-clean1 = clean1.apply(process_row, axis=1)
 
 # %%
-clean1
-
-# %%
-filtered_df = clean1[clean1['Value_derived'].isna()]
-grouped_df = filtered_df.groupby(['OBSERVATIONCODE', 'upvalue', 'Value_derived']).size().reset_index(name='c')
-unassigned = grouped_df.sort_values(by='c', ascending=False)
-
-
-# %%
-clean2 = obs_sup2.copy()
-clean2['Value_derived'] = np.nan
-clean2['SubValue1'] = ''
-
-clean2.loc[clean2['upvalue'].str.contains('\.\.', regex=False), 'Value_derived'] = clean2['upvalue'].str.replace('..', '.', regex=False)
-clean2.loc[clean2['upvalue'].str.contains('50|75|100'), 'Value_derived'] = clean2['upvalue'].str[:4].str.replace('[A-Z<>/\\ (]', '', regex=True)
-clean2.loc[clean2['upvalue'].str.contains('G'), 'Value_derived'] = clean2['upvalue'].str[:clean2['upvalue'].str.index('G')].str.replace('[A-Z<>/\\ (]', '', regex=True)
-
-clean2.loc[clean2['upvalue'].str.contains('5O', regex=False), 'Value_derived'] = 50
-clean2.loc[clean2['upvalue'].str.contains('\+', regex=False), 'Value_derived'] = clean2['upvalue'].apply(
-    lambda x: float(x[:x.index('+')]) + float(x[x.index('+')+1:x.index('+')+4])
-)
-clean2['Value_derived'] = pd.to_numeric(clean2['Value_derived'], errors='coerce')
-
-
-
-# %%
-unassigned = clean2[clean2['Value_derived'].isna()]
-unassigned = unassigned.groupby(['OBSERVATIONCODE', 'upvalue', 'Value_derived']).size().reset_index(name='c')
-unassigned = unassigned.sort_values(by='c', ascending=False)
-
-# %%
-cleaned_glucose = pd.concat([clean1, clean2], ignore_index=True)
-
-# %%
-myobs_sup = pd.read_sas("S:\\HDSB\\Projects\\Olis_cleaning\\indata\\olis_glucose_top5000.sas7bdat", encoding="latin1")
-
 def convert_value(value):
     if value.replace('.', '').isdigit():
         return float(value)
     else:
         return 99999999
 
-myobs_sup['num_value'] = myobs_sup['VALUE'].apply(convert_value)
-
 
 # %%
-myobs_sup
-
-#%%
-cleaned_glucose
-
-# %%
-glucose = pd.merge(myobs_sup, cleaned_glucose[['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST','Value_derived','SubValue1']], how='left',
-                   on=['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST'])
-
 def calculate_value(row):
     if row['num_value'] == 99999999:
         return row['Value_derived']
     else:
         return row['num_value']
 
-glucose['Value_derived_d'] = glucose.apply(calculate_value, axis=1)
-glucose['SubValue_derived_d'] = glucose['SubValue1']
-glucose['Value_recommended_d'] = glucose.apply(calculate_value, axis=1)
-glucose['SubValue_recommended_d'] = glucose['SubValue1']
-
-glucose=glucose.drop(columns=['Value_derived','SubValue1'], axis=1)
 
 
-# %%
-trim_bottom = 2  
-trim_top = 21.9  
-glucose.loc[
-    (~glucose['Value_derived_d'].between(trim_bottom, trim_top)) & (glucose['OBSERVATIONCODE'] != '4269-7') |
-    (glucose['UNITS'] == 'HOURS'),
-    ['Value_recommended_d', 'SubValue_recommended_d']
-] = [np.nan, '']
 
 #%%
-glucose
+if __name__ == '__main__':
+
+    extracode = '4269-7'
+    df = pd.read_sas("S:\\HDSB\\Projects\\Olis_cleaning\\indata\\olis_glucose_top5000.sas7bdat", encoding="latin1")
+    data=df
+   
+    df['upvalue'] = df['VALUE'].str.upper()
+    df['value_encoded'] = df['VALUE']
+
+    # Creating obs_sup_0 
+    obs_sup_0 = df[df['VALUE'].str.contains(r'[^\d\s.]')]
+
+    # Creating obs_sup 
+    excluded_strings = [
+        'TEST NOT PERFORMED', 'DISREGARD', 'CORRECTED ON', 'NOT VALID', 
+        'ORIGINAL GLUCOSE', 'FLUID', 'DIABETES', 'NO SPECIMEN RECEIVED',
+        'NO SAMPLE RECEIVED', 'NOT RECEIVED', 'UNABLE TO COLLECT',
+        'NOT SUFFICIENT QUANTITY', 'INSUFFICIENT SAMPLE', 
+        'INSUFFICIENT QUANTITY', 'SPECIMEN UNSATISFACTORY', 'MISLABELLED',
+        'UNLABELLED SAMPLE', 'HAEMOLYZED', 'HEMOLYZED', 'HEMOLYSED',
+        'QUERY SPECIMEN INTEGRITY', 'CONTAMINATION', 'CONTAMINATED',
+        'STABILITY LIMIT EXCEEDED', 'AIR BUBBLES IN SAMPLE', 'WRONG TUBE',
+        'FAILED TO MEET THE REQUIREMENTS', 'DISCARDED', 'NOT PROCESSED',
+        'UNABLE TO PROCESS', 'REJECTION', 'SPECIMEN REJECTED', 
+        'HAS BEEN REJECTED', 'REJECTED SPECIMEN', 'UNSUITABLE', 'CANCELLED',
+        'CANCEL', 'UNABLE TO PERFORM', 'CANNOT BE PERFORMED', 'NOT PERFORMED',
+        'HAS NOT BEEN PERFORMED', 'NOT BEEN SIGNED', 'ERROR', 'NOT REPORTED',
+        'WRONG TEST CODE', 'RESULT UNAVAILABLE', 'NOT AVAILABLE', 
+        'NOT BE AVAILABLE', 'DELETE', 'INVALID', 'QUESTIONABLE RESULTS', 
+        'BELOW THE MINIMUM ACCEPTABLE LEVEL', 'DUPLICATE REQUEST', 
+        'DUPLICATE ORDER', 'REQUEST A REDRAW'
+    ]
+
+    obs_sup = obs_sup_0[
+        (obs_sup_0['OBSERVATIONCODE'] != extracode) & 
+        obs_sup_0['upvalue'].apply(lambda x: any(digit.isdigit() for digit in x)) & 
+        ~obs_sup_0['upvalue'].apply(lambda x: any(substring in x for substring in excluded_strings))
+    ]
+    obs_sup=obs_sup.drop(columns=['VALUE'])
+
+    obs_sup2 = df[
+        (df['OBSERVATIONCODE'] == extracode) &  # Filtering by ObservationCode
+        (df['upvalue'].str.isdigit()) &  # Checking if 'upvalue' contains any digit
+        ~df['upvalue'].str.contains(r'TEST NOT PERFORMED|DISREGARD|CORRECTED ON|NOT VALID|ORIGINAL GLUCOSE|FLUID|DIABETES')  # Excluding specific keywords
+    ]
+    obs_sup2 = obs_sup2.reset_index(drop=True)
+
+    obs_sup_ = obs_sup.apply(check_and_call_missing, axis=1)
+
+    clean = obs_sup_.apply(process_row, axis=1)
+    clean = pd.concat([obs_sup_, clean], axis=1)
+
+    df = data
+    df=df.rename(columns={'HCN_ENCRYPTED':'HCN_ENCRYPTED',
+                        'ORDERSID':'ORDERSID',
+                        'OBSERVATIONCODE':'OBSERVATIONCODE',
+                        'OBSERVATIONDATETIME':'OBSERVATIONDATETIME',
+                        'OBSERVATIONRELEASETS':'OBSERVATIONRELEASETS',
+                        'VALUETYPE':'VALUETYPE',
+                        'UNITS':'UNITS',
+                        'REFRANGE':'REFRANGE',
+                        'OBSERVATIONPOSINTESTREQUEST':'OBSERVATIONPOSINTESTREQUEST',
+                        'TESTREQUESTPOSITIONINORDER':'TESTREQUESTPOSITIONINORDER'})
+
+    clean_date = pd.merge(clean, df[['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST','OBSERVATIONDATETIME']], 
+                        how='left', 
+                        on=['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST'])
+    
+    clean1 = clean.copy()
+    # Apply the function row-wise
+    clean1 = clean1.apply(process_row2, axis=1)
+
+    filtered_df = clean1[clean1['Value_derived'].isna()]
+    grouped_df = filtered_df.groupby(['OBSERVATIONCODE', 'upvalue', 'Value_derived']).size().reset_index(name='c')
+    unassigned = grouped_df.sort_values(by='c', ascending=False)
+
+    clean2 = obs_sup2.copy()
+    clean2['Value_derived'] = np.nan
+    clean2['SubValue1'] = ''
+
+    clean2.loc[clean2['upvalue'].str.contains('\.\.', regex=False), 'Value_derived'] = clean2['upvalue'].str.replace('..', '.', regex=False)
+    clean2.loc[clean2['upvalue'].str.contains('50|75|100'), 'Value_derived'] = clean2['upvalue'].str[:4].str.replace('[A-Z<>/\\ (]', '', regex=True)
+    clean2.loc[clean2['upvalue'].str.contains('G'), 'Value_derived'] = clean2['upvalue'].str[:clean2['upvalue'].str.index('G')].str.replace('[A-Z<>/\\ (]', '', regex=True)
+
+    clean2.loc[clean2['upvalue'].str.contains('5O', regex=False), 'Value_derived'] = 50
+    clean2.loc[clean2['upvalue'].str.contains('\+', regex=False), 'Value_derived'] = clean2['upvalue'].apply(
+        lambda x: float(x[:x.index('+')]) + float(x[x.index('+')+1:x.index('+')+4])
+    )
+    clean2['Value_derived'] = pd.to_numeric(clean2['Value_derived'], errors='coerce')
+
+    unassigned = clean2[clean2['Value_derived'].isna()]
+    unassigned = unassigned.groupby(['OBSERVATIONCODE', 'upvalue', 'Value_derived']).size().reset_index(name='c')
+    unassigned = unassigned.sort_values(by='c', ascending=False)
+
+    cleaned_glucose = pd.concat([clean1, clean2], ignore_index=True)
+
+    myobs_sup = pd.read_sas("S:\\HDSB\\Projects\\Olis_cleaning\\indata\\olis_glucose_top5000.sas7bdat", encoding="latin1")
+    myobs_sup['num_value'] = myobs_sup['VALUE'].apply(convert_value)
+
+    glucose = pd.merge(myobs_sup, cleaned_glucose[['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST','Value_derived','SubValue1']], how='left',
+                   on=['ORDERSID', 'TESTREQUESTPOSITIONINORDER', 'OBSERVATIONPOSINTESTREQUEST'])
+    
+    glucose['Value_derived_d'] = glucose.apply(calculate_value, axis=1)
+    glucose['SubValue_derived_d'] = glucose['SubValue1']
+    glucose['Value_recommended_d'] = glucose.apply(calculate_value, axis=1)
+    glucose['SubValue_recommended_d'] = glucose['SubValue1']
+
+    glucose=glucose.drop(columns=['Value_derived','SubValue1'], axis=1)
+
+    trim_bottom = 2  
+    trim_top = 21.9  
+    glucose.loc[
+        (~glucose['Value_derived_d'].between(trim_bottom, trim_top)) & (glucose['OBSERVATIONCODE'] != '4269-7') |
+        (glucose['UNITS'] == 'HOURS'),
+        ['Value_recommended_d', 'SubValue_recommended_d']
+    ] = [np.nan, '']
+
+    # Note: Final output data is in the dataframe "glucose"
